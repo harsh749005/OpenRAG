87224312d89496a4be9e0dc752af0c098f243a40
Harsh <patelharsh749005@gmail.com>
2025-01-16 21:50:27 +0530

    icons and images are added

---
+++ src/assets/svg/Icon1.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../svgStyle/icon1.css';
+const Icon1 = () => {
+  return (
+    <>
+    <svg id="comp-kk2sktdt" preserveAspectRatio="xMidYMid meet" data-bbox="20.532 45.947 158.928 108.107" viewBox="20.532 45.947 158.928 108.107" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="color" role="presentation" aria-hidden="true" aria-label="">
+      <defs>
+        
+      </defs>
+      <g>
+        <path d="M82.935 75.852l44.836 20.692a4.833 4.833 0 0 0 4.048 0l44.836-20.692a4.828 4.828 0 0 0 0-8.768l-44.836-20.692a4.826 4.826 0 0 0-4.047 0L82.935 67.083a4.828 4.828 0 0 0 0 8.769zm46.86-19.759l33.312 15.374-33.312 15.374-33.312-15.374 33.312-15.374z" fill="#AD49BD" data-color="1"></path>
+        <path d="M172.608 95.616l-42.813 19.758-42.813-19.758a4.83 4.83 0 0 0-4.047 8.769l44.836 20.692a4.833 4.833 0 0 0 4.048 0l44.836-20.692a4.83 4.83 0 0 0-4.047-8.769z" fill="#C86BCF" data-color="1"></path>
+        <path d="M172.608 124.149l-42.813 19.758-42.813-19.758a4.828 4.828 0 1 0-4.047 8.769l44.836 20.692a4.833 4.833 0 0 0 4.048 0l44.836-20.692a4.83 4.83 0 0 0-4.047-8.769z" fill="#8C2C99" data-color="1"></path>
+        <path d="M65.095 95.171H25.368a4.829 4.829 0 1 0 0 9.658h39.727a4.829 4.829 0 1 0 0-9.658z" fill="#F06292" data-color="1"></path>
+        <path d="M65.095 76.446H37.44a4.829 4.829 0 1 0 0 9.658h27.655a4.829 4.829 0 1 0 0-9.658z" fill="#6759FF" data-color="1"></path>
+        <path d="M65.095 113.897H49.512a4.829 4.829 0 1 0 0 9.658h15.583a4.829 4.829 0 1 0 0-9.658z" fill="#AD49BD" data-color="1"></path>
+      </g>
+    </svg>
+    </>
+  )
+}
+
+export default Icon1
