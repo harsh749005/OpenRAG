46965ed14cd2cf58fcc835237ac4cdc8cdc934b1
Harsh <patelharsh749005@gmail.com>
2025-01-16 21:51:04 +0530

    some changes and some stylesheets are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState,useRef } from 'react'
-import React from 'react';
-import Blogs from './pages/Blogs';
-
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import GetinTouch from "./pages/GetinTouch";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import FAQs from "./pages/FAQs";
+import Products from "./pages/Products";
+import Blogs from "./pages/Blogs";
+import ReadBlogs from "./pages/ReadBlogs";
 function App() {
   const [count, setCount] = useState(0)
  
   return (
     <div className='w-full h-full bg-black'>
-    <Blogs/>
+         
+        {/* Navbar and other sections */}
+        <Navbar />
+        <Routes>
+          {/* Home Route */}
+          <Route path="/OpenRAG"element={<Home/>}/>
+          {/* Get in Touch Route */}
+          <Route path="/OpenRAG/get-in-touch" element={<GetinTouch />} />
+          <Route path="/OpenRAG/events" element={<Events/>}/>
+          <Route path="/OpenRAG/blogs" element={<Blogs/>}/>
+          <Route path="/OpenRAG/products" element={<Products/>}/>
+          <Route path="/OpenRAG/FAQ" element={<FAQs/>}/>
+          <Route path="/OpenRAG/readblogs" element={<ReadBlogs/>}/>
+        </Routes>
       
     </div>
   )
