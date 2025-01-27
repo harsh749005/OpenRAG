9174324e1a84df0d0315535cd044229f4b850d1a
Harsh <patelharsh749005@gmail.com>
2025-01-28 01:41:32 +0530

    withCredentials removed

--- frontend/src/pages/GetinTouch.jsx
+++ frontend/src/pages/GetinTouch.jsx
@@ -44,7 +44,7 @@ const GetinTouch = () => {
       description,
     }
     // Send form data to your server here
-    axios.defaults.withCredentials = true;
+    // axios.defaults.withCredentials = true;
     axios.post(`${backendBaseUrl}/create`, values)
      .then((response) => {
         
